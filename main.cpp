9a8e13b768368d2231fa9d59b66ed50eeac4ef87
Holger Ackermann <github@schwedenstuhl.de>
2021-07-16 18:21:41 +0200

    Added application icon.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "mainwindow.h"
-
+#include <QLocale>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/qnonogram.ico"));
     MainWindow w;
     w.show();
     return a.exec();
